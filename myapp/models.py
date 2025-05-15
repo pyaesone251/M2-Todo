e93232a7b5078a1fa193c3b9f6390b13cb4ebaeb
pyaesone251 <pyaesonepaing06@gmail.com>
2025-05-15 15:27:39 +0630

    todo_update

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoappModel(models.Model):
+    task = models.CharField(max_length=200)
+    note = models.TextField()
+    complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import TodoappModel
 
 # Create your views here.
 def indexview(request):
-    return render(request, 'index.html')
+    task = TodoappModel.objects.all()
+    contex = {
+        'task':task
+    }
+    return render(request, 'index.html',contex)
